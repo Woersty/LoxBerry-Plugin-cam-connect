7a3fc3bcc162a019331eaaa06b9048dd2f935ca8
Wörsty <git@loxberry.woerstenfeld.de>
2016-09-24 19:13:49 +0200

    Zuviel gelöscht... :)

--- postupgrade.sh
+++ postupgrade.sh
@@ -23,6 +23,13 @@
 # <ERROR> This is an error!"
 # <FAIL> This is a fail!"
 
+ARGV0=$0 # Zero argument is shell command
+ARGV1=$1 # First argument is temp folder during install
+ARGV2=$2 # Second argument is Plugin-Name for scipts etc.
+ARGV3=$3 # Third argument is Plugin installation folder
+ARGV4=$4 # Forth argument is Plugin version
+ARGV5=$5 # Fifth argument is Base folder of LoxBerry
+
 echo "<INFO> Copy back existing config files"
 cp -v -r /tmp/$ARGV1\_upgrade/config/$ARGV3/* $ARGV5/config/plugins/$ARGV3/ 
 
