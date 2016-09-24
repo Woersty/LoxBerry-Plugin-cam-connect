9cd76aabcd9c488a16f793258289d144588dced2
Wörsty <git@loxberry.woerstenfeld.de>
2016-09-24 19:08:28 +0200

    Konfiguration bei Upgrade retten

--- postupgrade.sh
+++ postupgrade.sh
@@ -23,24 +23,15 @@
 # <ERROR> This is an error!"
 # <FAIL> This is a fail!"
 
-# To use important variables from command line use the following code:
-ARGV0=$0 # Zero argument is shell command
-#echo "<INFO> Command is: $ARGV0"
+echo "<INFO> Copy back existing config files"
+cp -v -r /tmp/$ARGV1\_upgrade/config/$ARGV3/* $ARGV5/config/plugins/$ARGV3/ 
 
-ARGV1=$1 # First argument is temp folder during install
-#echo "<INFO> Temporary folder is: $ARGV1"
+echo "<INFO> Copy back existing log files"
+cp -v -r /tmp/$ARGV1\_upgrade/log/$ARGV3/* $ARGV5/log/plugins/$ARGV3/ 
 
-ARGV2=$2 # Second argument is Plugin-Name for scipts etc.
-#echo "<INFO> (Short) Name is: $ARGV2"
+echo "<INFO> Remove temporary folders"
+rm -r /tmp/$ARGV1\_upgrade
 
-ARGV3=$3 # Third argument is Plugin installation folder
-#echo "<INFO> Installation folder is: $ARGV3"
-
-ARGV4=$4 # Forth argument is Plugin version
-#echo "<INFO> Installation folder is: $ARGV4"
-
-ARGV5=$5 # Fifth argument is Base folder of LoxBerry
-#echo "<INFO> Base folder is: $ARGV5"
 
 # Exit with Status 0
 exit 0
--- preupgrade.sh
+++ preupgrade.sh
@@ -22,24 +22,16 @@
 # <ERROR> This is an error!"
 # <FAIL> This is a fail!"
 
-# To use important variables from command line use the following code:
-ARGV0=$0 # Zero argument is shell command
-#echo "<INFO> Command is: $ARGV0"
+echo "<INFO> Creating temporary folders for upgrading"
+mkdir -p /tmp/$ARGV1\_upgrade
+mkdir -p /tmp/$ARGV1\_upgrade/config
+mkdir -p /tmp/$ARGV1\_upgrade/log
 
-ARGV1=$1 # First argument is temp folder during install
-#echo "<INFO> Temporary folder is: $ARGV1"
+echo "<INFO> Backing up existing config files"
+cp -v -r $ARGV5/config/plugins/$ARGV3/ /tmp/$ARGV1\_upgrade/config
 
-ARGV2=$2 # Second argument is Plugin-Name for scipts etc.
-#echo "<INFO> (Short) Name is: $ARGV2"
-
-ARGV3=$3 # Third argument is Plugin installation folder
-#echo "<INFO> Installation folder is: $ARGV3"
-
-ARGV4=$4 # Forth argument is Plugin version
-#echo "<INFO> Installation folder is: $ARGV4"
-
-ARGV5=$5 # Fifth argument is Base folder of LoxBerry
-#echo "<INFO> Base folder is: $ARGV5"
+echo "<INFO> Backing up existing log files"
+cp -v -r $ARGV5/log/plugins/$ARGV3/ /tmp/$ARGV1\_upgrade/log
 
 # Exit with Status 0
 exit 0
