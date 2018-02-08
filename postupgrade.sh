d7bf4dc28319cc3e17ccbe0a8729b2276d19bac9
Wörsty <git@loxberry.woerstenfeld.de>
2018-02-08 23:39:30 +0100

    Save config during updates

--- postupgrade.sh
+++ postupgrade.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+ARGV0=$0 # Zero argument is shell command
+ARGV1=$1 # First argument is temp folder during install
+ARGV2=$2 # Second argument is Plugin-Name for scipts etc.
+ARGV3=$3 # Third argument is Plugin installation folder
+ARGV4=$4 # Forth argument is Plugin version
+ARGV5=$5 # Fifth argument is Base folder of LoxBerry
+
+echo "<INFO> Copy back existing config files"
+cp -v -r /var/tmp/$ARGV3/* $ARGV5/config/plugins/$ARGV3/ 
+
+echo "<INFO> Remove temporary folders"
+rm -rf /var/tmp/$ARGV3
+
 exit 0
--- preupgrade.sh
+++ preupgrade.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
+# To use important variables from command line use the following code:
+ARGV0=$0 # Zero argument is shell command
+#echo "<INFO> Command is: $ARGV0"
+
+ARGV1=$1 # First argument is temp folder during install
+#echo "<INFO> Temporary folder is: $ARGV1"
+
+ARGV2=$2 # Second argument is Plugin-Name for scipts etc.
+#echo "<INFO> (Short) Name is: $ARGV2"
+
+ARGV3=$3 # Third argument is Plugin installation folder
+#echo "<INFO> Installation folder is: $ARGV3"
+
+ARGV4=$4 # Forth argument is Plugin version
+#echo "<INFO> Installation folder is: $ARGV4"
+
+ARGV5=$5 # Fifth argument is Base folder of LoxBerry
+#echo "<INFO> Installation folder is: $ARGV5"
+
+echo "<INFO> Creating temporary folders for upgrading"
+mkdir -p /var/tmp/$ARGV3
+
+echo "<INFO> Backing up existing config files"
+cp -v -r $ARGV5/config/plugins/$ARGV3/ /var/tmp/$ARGV3
+
 exit 0
